4b1a7887f9fb37d5e57f1cb4bc87c01f7485b16d
Claudson Oliveira <claudsonweb@gmail.com>
2023-08-21 23:34:26 +0100

    give more food to the database

--- db/seed.sql
+++ db/seed.sql
@@ -1,7 +1,7 @@
 ALTER SYSTEM SET max_connections = 1000;
 CREATE EXTENSION pg_trgm;
 ALTER DATABASE rinha SET synchronous_commit=OFF;
-ALTER SYSTEM SET shared_buffers TO "375MB";
+ALTER SYSTEM SET shared_buffers TO "425MB";
 
 
 create table if not exists pessoas(

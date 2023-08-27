52cfad454fc06e416d502901662a8fdadb29a81e
Claudson Oliveira <claudsonweb@gmail.com>
2023-08-27 14:05:27 +0100

    reduce workers

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"os"
+	"runtime"
 	"time"
 )
 
@@ -9,10 +10,11 @@ var DatabaseURL string
 var CacheURL string
 var PROFILING bool
 var NumBatch = 100
-var NumWorkers = 100
+var NumWorkers = 1
 var WorkerTimeout = 1 * time.Second
 
 func init() {
+	NumWorkers = runtime.GOMAXPROCS(0) * 2
 	DatabaseURL = envOrFatal("DATABASE_URL")
 	CacheURL = envOrFatal("CACHE_URL")
 	PROFILING = os.Getenv("PROFILING") == "true"
